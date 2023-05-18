f8945467b31b00587df26e43031359aafe541097
Daniel Knopf <daniel.knopf@intel.com>
2023-05-18 16:40:11 +0200

    Configuration, SDL setup and first steps into SDL rendering

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "SDL2/SDL.h"
+
+int main(int argc, char** argv)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window = SDL_CreateWindow(
+        "CHIP-8 Window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640, 320,
+        SDL_WINDOW_SHOWN
+    );
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
+
+    while(1)
+    {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+    }
+
+    SDL_DestroyWindow(window);
+    return 0;
+}
